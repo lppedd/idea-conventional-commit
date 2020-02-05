7ddc0fea6428b2e7fec5e9422e0e768dccffb1d9
Edoardo Luppi <lp.edoardo@gmail.com>
2020-02-05 22:37:04 +0100

    build: simplify build script and use Kotlin EAP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,20 @@
-import org.jetbrains.intellij.tasks.PatchPluginXmlTask
-
 plugins {
   java
   id("org.jetbrains.intellij") version "0.4.15"
-  kotlin("jvm") version "1.3.61"
+  kotlin("jvm") version "1.3.70-eap-184"
 }
 
 group = "com.github.lppedd"
 version = "0.4.0"
 
 repositories {
-  mavenCentral()
+  maven("https://dl.bintray.com/kotlin/kotlin-eap")
   maven("https://jitpack.io")
+  mavenCentral()
 }
 
 dependencies {
-  implementation(kotlin("stdlib-jdk8", "1.3.61"))
+  implementation(kotlin("stdlib-jdk8", "1.3.70-eap-184"))
   implementation("org.json", "json", "20190722")
   implementation("com.github.everit-org.json-schema", "org.everit.json.schema", "1.12.1")
   testCompile("junit:junit:4.12")
@@ -28,10 +27,20 @@ configure<JavaPluginConvention> {
 tasks {
   compileKotlin {
     kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.freeCompilerArgs += "-XXLanguage:+NewInference"
   }
 
   compileTestKotlin {
     kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.freeCompilerArgs += "-XXLanguage:+NewInference"
+  }
+
+  patchPluginXml {
+    version(project.version)
+    sinceBuild("192")
+    untilBuild("201.*")
+    pluginDescription(File("plugin-description.html").readText(Charsets.UTF_8))
+    changeNotes(File("change-notes/${version.replace('.', '_')}.html").readText(Charsets.UTF_8))
   }
 }
 
@@ -40,12 +49,4 @@ intellij {
   downloadSources = true
   pluginName = "Conventional Commit"
   setPlugins("java")
-}
-
-tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
-  version(project.version)
-  sinceBuild("192")
-  untilBuild("201.*")
-  pluginDescription(File("plugin-description.html").readText(Charsets.UTF_8))
-  changeNotes(File("change-notes/${version.replace('.', '_')}.html").readText(Charsets.UTF_8))
 }
