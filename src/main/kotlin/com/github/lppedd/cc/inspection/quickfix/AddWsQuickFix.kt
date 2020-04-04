d972a943744fd3a6a737361f61b77d102259e9c5
Edoardo Luppi <lp.edoardo@gmail.com>
2020-04-04 20:27:37 +0200

    refactor: add quick fix to insert space before subject

---
+++ src/main/kotlin/com/github/lppedd/cc/inspection/quickfix/AddWsQuickFix.kt
@@ -0,0 +1,20 @@
+package com.github.lppedd.cc.inspection.quickfix
+
+import com.github.lppedd.cc.CCBundle
+import com.github.lppedd.cc.inspection.ConventionalCommitBaseQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.Project
+
+/**
+ * @author Edoardo Luppi
+ */
+internal class AddWsQuickFix(private val toAdd: Int) :
+    ConventionalCommitBaseQuickFix(CCBundle["cc.inspection.nonStandardMessage.addWs"]) {
+  override fun doApplyFix(project: Project, document: Document, descriptor: ProblemDescriptor?) {
+    descriptor?.let {
+      val start = it.textRangeInElement.startOffset
+      document.insertString(start, " ".repeat(toAdd))
+    }
+  }
+}
