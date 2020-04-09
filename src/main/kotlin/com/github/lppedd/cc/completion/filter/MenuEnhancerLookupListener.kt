aa2205302251307db300bcc4a19ac1c27344c446
Edoardo Luppi <lp.edoardo@gmail.com>
2020-04-09 12:11:33 +0200

    refactor: reset filter actions state an each lookup refresh

--- src/main/kotlin/com/github/lppedd/cc/completion/filter/MenuEnhancerLookupListener.kt
+++ src/main/kotlin/com/github/lppedd/cc/completion/filter/MenuEnhancerLookupListener.kt
@@ -1,11 +1,17 @@
 package com.github.lppedd.cc.completion.filter
 
 import com.github.lppedd.cc.api.CommitTokenProvider
+import com.github.lppedd.cc.emptyCollection
 import com.intellij.codeInsight.lookup.LookupEvent
 import com.intellij.codeInsight.lookup.LookupListener
 import com.intellij.codeInsight.lookup.impl.LookupImpl
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.actionSystem.impl.ActionButton
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.util.ReflectionUtil.getField
 import org.jetbrains.annotations.ApiStatus
 import java.util.concurrent.atomic.AtomicBoolean
@@ -16,11 +22,15 @@ private val IS_MENU_MODIFIED = AtomicBoolean(false)
  * @author Edoardo Luppi
  */
 @ApiStatus.Internal
-internal class MenuEnhancerLookupListener(private val lookup: LookupImpl) : LookupListener {
-  private var providers: Collection<CommitTokenProvider> = emptyList()
+internal class MenuEnhancerLookupListener(private val lookup: LookupImpl) : LookupListener, AnActionListener {
+  private var shouldAcceptActionsChanges = true
+  private var providers = emptyCollection<CommitTokenProvider>()
+  private var actions = emptyCollection<FilterProviderAction>()
+  private var messageBus = ApplicationManager.getApplication().messageBus.connect()
 
   init {
     lookup.addLookupListener(this)
+    messageBus.subscribe(AnActionListener.TOPIC, this)
   }
 
   fun setProviders(providers: Collection<CommitTokenProvider>) {
@@ -30,6 +40,10 @@ internal class MenuEnhancerLookupListener(private val lookup: LookupImpl) : Look
   override fun uiRefreshed() {
     try {
       if (IS_MENU_MODIFIED.get() && lookup.isShown) {
+        if (shouldAcceptActionsChanges) {
+          actions.forEach(FilterProviderAction::reset)
+        }
+
         return
       }
 
@@ -40,9 +54,8 @@ internal class MenuEnhancerLookupListener(private val lookup: LookupImpl) : Look
       val myMenuButton = getField<ActionButton>(myUi.javaClass, myUi, null, "myMenuButton")
       val menuActions = myMenuButton.action as DefaultActionGroup
 
-      providers
-        .map { FilterProviderAction(lookup, it) }
-        .forEach(menuActions::add)
+      actions = providers.map { FilterProviderAction(lookup, it) }
+      actions.forEach(menuActions::add)
     } catch (ignored: ReflectiveOperationException) {
       // This should never happen, but in case I can't do anything about it,
       // so I'll just clean-up and let the user continue without applying any change
@@ -54,9 +67,22 @@ internal class MenuEnhancerLookupListener(private val lookup: LookupImpl) : Look
     cleanUp()
   }
 
+  override fun beforeActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
+    if (action is FilterProviderAction) {
+      shouldAcceptActionsChanges = false
+    }
+  }
+
+  override fun afterActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
+    if (action is FilterProviderAction) {
+      shouldAcceptActionsChanges = true
+    }
+  }
+
   private fun cleanUp() {
     IS_MENU_MODIFIED.set(false)
+    providers = emptyCollection()
     lookup.removeLookupListener(this)
-    providers = emptyList()
+    messageBus.disconnect()
   }
 }
