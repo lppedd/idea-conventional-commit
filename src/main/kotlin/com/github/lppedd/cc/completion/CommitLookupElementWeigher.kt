0a34d65034f4dfed04051b76e11126df3c535768
Edoardo Luppi <lp.edoardo@gmail.com>
2020-04-09 12:36:00 +0200

    refactor: simplify CommitLookupElementWeigher

--- src/main/kotlin/com/github/lppedd/cc/completion/CommitLookupElementWeigher.kt
+++ src/main/kotlin/com/github/lppedd/cc/completion/CommitLookupElementWeigher.kt
@@ -3,7 +3,6 @@ package com.github.lppedd.cc.completion
 import com.github.lppedd.cc.lookupElement.CommitLookupElement
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementWeigher
-import com.intellij.openapi.project.Project
 
 // This is just a big number
 private const val MAX_ELEMENTS_PER_PROVIDER = 1_000
@@ -11,13 +10,7 @@ private const val MAX_ELEMENTS_PER_PROVIDER = 1_000
 /**
  * @author Edoardo Luppi
  */
-internal class CommitLookupElementWeigher : LookupElementWeigher {
-  private val project: Project
-
-  constructor(project: Project) : super("commitLookupElementWeigher") {
-    this.project = project
-  }
-
+internal object CommitLookupElementWeigher : LookupElementWeigher("commitLookupElementWeigher") {
   override fun weigh(element: LookupElement): Comparable<*> =
     if (element is CommitLookupElement) {
       // Lower priority = less weight = higher-up in the list
