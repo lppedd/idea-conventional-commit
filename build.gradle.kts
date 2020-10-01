758296b668d17ca3db78e9b5b47afc088f0c9395
Edoardo Luppi <lp.edoardo@gmail.com>
2020-10-01 23:54:36 +0200

    build: update compatibility up to IDEA 2020.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ tasks {
   patchPluginXml {
     version(project.version)
     sinceBuild("192.5728")
-    untilBuild("202.*")
+    untilBuild("203.*")
     pluginDescription(File("plugin-description.html").readText(Charsets.UTF_8))
     changeNotes(File("change-notes/${version.replace('.', '_')}.html").readText(Charsets.UTF_8))
   }
