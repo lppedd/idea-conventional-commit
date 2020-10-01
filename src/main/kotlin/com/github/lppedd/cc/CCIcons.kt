64837f861f5466e593b6f80f8c0fb155c9b7123f
Edoardo Luppi <lp.edoardo@gmail.com>
2020-10-01 23:39:55 +0200

    refactor: avoid deprecated method IconLoader.getIcon(String)

--- src/main/kotlin/com/github/lppedd/cc/CCIcons.kt
+++ src/main/kotlin/com/github/lppedd/cc/CCIcons.kt
@@ -2,15 +2,17 @@
 
 package com.github.lppedd.cc
 
-import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.util.IconLoader.getIcon
 import javax.swing.Icon
 
-@JvmField val ICON_UNKNOWN_PROVIDER: Icon = IconLoader.getIcon("/icons/unknownProvider.svg")
-@JvmField val ICON_DEFAULT_PRESENTATION: Icon = IconLoader.getIcon("/icons/defaultPresentation.svg")
-@JvmField val ICON_TYPE: Icon = IconLoader.getIcon("/icons/commitType.svg")
-@JvmField val ICON_SCOPE: Icon = IconLoader.getIcon("/icons/commitScope.svg")
-@JvmField val ICON_SUBJECT: Icon = IconLoader.getIcon("/icons/commitDescription.svg")
-@JvmField val ICON_BODY: Icon = IconLoader.getIcon("/icons/commitBody.svg")
-@JvmField val ICON_FOOTER: Icon = IconLoader.getIcon("/icons/commitFooter.svg")
-@JvmField val ICON_ARROW_RIGHT: Icon = IconLoader.getIcon("/icons/arrowRight.svg")
-@JvmField val ICON_DISABLED: Icon = IconLoader.getIcon("/icons/defaultPresentationDisabled.svg")
+private val THIS_CLASS = Class.forName("com.github.lppedd.cc.CCIcons")
+
+@JvmField val ICON_UNKNOWN_PROVIDER: Icon = getIcon("/icons/unknownProvider.svg", THIS_CLASS)
+@JvmField val ICON_DEFAULT_PRESENTATION: Icon = getIcon("/icons/defaultPresentation.svg", THIS_CLASS)
+@JvmField val ICON_TYPE: Icon = getIcon("/icons/commitType.svg", THIS_CLASS)
+@JvmField val ICON_SCOPE: Icon = getIcon("/icons/commitScope.svg", THIS_CLASS)
+@JvmField val ICON_SUBJECT: Icon = getIcon("/icons/commitDescription.svg", THIS_CLASS)
+@JvmField val ICON_BODY: Icon = getIcon("/icons/commitBody.svg", THIS_CLASS)
+@JvmField val ICON_FOOTER: Icon = getIcon("/icons/commitFooter.svg", THIS_CLASS)
+@JvmField val ICON_ARROW_RIGHT: Icon = getIcon("/icons/arrowRight.svg", THIS_CLASS)
+@JvmField val ICON_DISABLED: Icon = getIcon("/icons/defaultPresentationDisabled.svg", THIS_CLASS)
