002885672e45a9e2fa0455b2950d96de43712df6
Edoardo Luppi <lp.edoardo@gmail.com>
2020-10-20 14:20:25 +0200

    refactor: show what's new dialog only if installed vers. is greater than last registered vers.

--- src/main/kotlin/com/github/lppedd/cc/whatsnew/DefaultWhatsNewProvider.kt
+++ src/main/kotlin/com/github/lppedd/cc/whatsnew/DefaultWhatsNewProvider.kt
@@ -5,6 +5,8 @@ import com.intellij.ide.plugins.IdeaPluginDescriptor
 import com.intellij.ide.plugins.PluginManager
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.extensions.PluginId
+import java.util.*
+import kotlin.math.max
 
 /**
  * Provides the changelogs for the Conventional Commit core plugin.
@@ -19,17 +21,47 @@ private class DefaultWhatsNewProvider : WhatsNewProvider() {
 
   override fun shouldDisplay(): Boolean {
     val properties = PropertiesComponent.getInstance()
-    val pluginInstalledVersion = getPlugin()?.version ?: return false
-    val pluginRegisteredVersion = properties.getValue(versionPropertyName)
+    val installedVersion = getPlugin()?.version ?: return false
+    val registeredVersion = properties.getValue(versionPropertyName) ?: return true
 
-    if (pluginInstalledVersion == pluginRegisteredVersion) {
-      return false
+    // We display the dialog only if the installed version
+    // is greater than the last registered version
+    if (PluginVersion(installedVersion) > PluginVersion(registeredVersion)) {
+      properties.setValue(versionPropertyName, installedVersion)
+      return true
     }
 
-    properties.setValue(versionPropertyName, pluginInstalledVersion)
-    return true
+    return false
   }
 
   private fun getPlugin(): IdeaPluginDescriptor? =
     PluginManager.getPlugin(PluginId.findId("com.github.lppedd.idea-conventional-commit"))
+
+  private class PluginVersion(version: String) : Comparable<PluginVersion> {
+    private val parts = version.split(".").map(String::toInt)
+
+    init {
+      require(parts.isNotEmpty()) { "The plugin version is invalid" }
+    }
+
+    override fun equals(other: Any?): Boolean {
+      return compareTo(other as? PluginVersion ?: return false) == 0
+    }
+
+    override fun hashCode(): Int =
+      Objects.hash(parts)
+
+    override fun compareTo(other: PluginVersion): Int {
+      val maxParts = max(parts.size, other.parts.size)
+
+      for (i in 0 until maxParts) {
+        val thisPart = if (i < parts.size) parts[i] else 0
+        val otherPart = if (i < other.parts.size) other.parts[i] else 0
+        if (thisPart < otherPart) return -1
+        if (thisPart > otherPart) return 1
+      }
+
+      return 0
+    }
+  }
 }
