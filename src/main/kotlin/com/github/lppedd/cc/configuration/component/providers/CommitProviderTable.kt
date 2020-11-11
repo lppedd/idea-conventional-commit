e5cafa500846dca749e1aca4bb6707b1f61ffac2
Edoardo Luppi <lp.edoardo@gmail.com>
2020-11-11 15:24:38 +0100

    refactor: move CommitProviderRenderer inside CommitProviderTable

--- src/main/kotlin/com/github/lppedd/cc/configuration/component/providers/CommitProviderTable.kt
+++ src/main/kotlin/com/github/lppedd/cc/configuration/component/providers/CommitProviderTable.kt
@@ -1,15 +1,31 @@
 package com.github.lppedd.cc.configuration.component.providers
 
 import com.github.lppedd.cc.CCBundle
+import com.github.lppedd.cc.CCIcons
 import com.github.lppedd.cc.api.CommitTokenProvider
 import com.github.lppedd.cc.ui.CCTable
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.ui.ColoredTableCellRenderer
+import com.intellij.ui.scale.JBUIScale
+import com.intellij.ui.speedSearch.SpeedSearchUtil
+import com.intellij.util.ui.JBUI
+import javax.swing.Icon
+import javax.swing.JTable
 
 /**
  * @author Edoardo Luppi
  */
 internal class CommitProviderTable<T : CommitTokenProvider> : CCTable() {
-  companion object {
-    const val COLUMN_PROVIDER = 0
+  private companion object {
+    private const val COLUMN_PROVIDER = 0
+
+    private val iconSize = ApplicationInfo.getInstance().let {
+      if ("${it.majorVersion}.${it.minorVersion}" == "2019.2") {
+        16f
+      } else {
+        32f
+      }
+    }
   }
 
   private val model = CommitProviderModel<T>()
@@ -41,5 +57,43 @@ internal class CommitProviderTable<T : CommitTokenProvider> : CCTable() {
 
   fun reset() {
     model.setProviders(latest.toMutableList())
+  }
+
+  private class CommitProviderRenderer : ColoredTableCellRenderer() {
+    override fun isTransparentIconBackground(): Boolean =
+      true
+
+    override fun customizeCellRenderer(
+        table: JTable,
+        value: Any?,
+        isSelected: Boolean,
+        hasFocus: Boolean,
+        row: Int,
+        column: Int,
+    ) {
+      border = JBUI.Borders.empty()
+      ipad = JBUI.insets(4, 6)
+      setValue(value)
+      SpeedSearchUtil.applySpeedSearchHighlighting(table, this, true, isSelected)
+    }
+
+    private fun setValue(value: Any?) {
+      if (value is CommitTokenProvider) {
+        val (name, icon) = value.getPresentation()
+        setIcon(getIcon(icon))
+        append(name)
+      }
+    }
+
+    private fun getIcon(icon: Icon): Icon {
+      val scaledHW = JBUIScale.scale(iconSize)
+      return if (icon.iconHeight <= scaledHW && icon.iconWidth <= scaledHW) {
+        icon
+      } else {
+        // The icon doesn't match the size prerequisite.
+        // Thus we display a generic one
+        CCIcons.Provider.Unknown
+      }
+    }
   }
 }
