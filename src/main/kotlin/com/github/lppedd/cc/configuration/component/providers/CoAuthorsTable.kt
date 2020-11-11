1ab8ef6b22c6aa143387df1aeae2ae096cde2766
Edoardo Luppi <lp.edoardo@gmail.com>
2020-11-11 18:41:33 +0100

    refactor: correctly calculate minimum column width for checkbox when table is empty

--- src/main/kotlin/com/github/lppedd/cc/configuration/component/providers/CoAuthorsTable.kt
+++ src/main/kotlin/com/github/lppedd/cc/configuration/component/providers/CoAuthorsTable.kt
@@ -126,7 +126,12 @@ internal class CoAuthorsTable(tableModel: CoAuthorsTableModel) : CCTable(tableMo
   }
 
   private fun resizeColumnsWidthToFitContent() {
-    val minimumWidth = 15
+    // When the table is empty this minimumWidth value is used
+    // as the minimum width for the checkbox column.
+    // Earlier it was set to 15px, which became a mess with scaling,
+    // while now we try to calculate the minimum width using a JCheckBox instance,
+    // or we fallback to a scaled value
+    val minimumWidth = max(JBUIScale.scale(19), JBCheckBox().minimumSize.width + 1)
     val maxWidth = 300
     val columnModel = columnModel
 
