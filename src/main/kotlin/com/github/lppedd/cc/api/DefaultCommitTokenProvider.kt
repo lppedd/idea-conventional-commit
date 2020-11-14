a7e102f3d27db0dae3c2adf8a4038962fb805e32
Edoardo Luppi <lp.edoardo@gmail.com>
2020-11-14 20:25:00 +0100

    refactor: move retrieval of recently used footer values under VCS dependency

--- src/main/kotlin/com/github/lppedd/cc/api/DefaultCommitTokenProvider.kt
+++ src/main/kotlin/com/github/lppedd/cc/api/DefaultCommitTokenProvider.kt
@@ -5,18 +5,10 @@ import com.github.lppedd.cc.CCIcons
 import com.github.lppedd.cc.CCNotificationService
 import com.github.lppedd.cc.configuration.CCConfigService
 import com.github.lppedd.cc.configuration.CCDefaultTokensService
-import com.github.lppedd.cc.parser.CCParser
-import com.github.lppedd.cc.parser.FooterTokens
-import com.github.lppedd.cc.parser.ValidToken
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.VcsConfiguration
 import org.everit.json.schema.ValidationException
 import org.jetbrains.annotations.ApiStatus.*
-import kotlin.text.RegexOption.MULTILINE
-
-private val BEGIN_END_WS_REGEX = Regex("""^\s+|\s+$""")
-private val BLANK_LINES_REGEX = Regex("""^\s*$""", MULTILINE)
 
 /**
  * @author Edoardo Luppi
@@ -63,42 +55,12 @@ internal class DefaultCommitTokenProvider(private val project: Project) :
       commitType: String?,
       commitScope: String?,
       commitSubject: String?,
-  ): Collection<CommitFooterValue> {
-    val isCoAuthoredBy = "co-authored-by".equals(footerType, true)
-    val lastN = if (isCoAuthoredBy) 5 else 15
-    val recentValues = VcsConfiguration.getInstance(project)
-      .recentMessages
-      .asReversed()
-      .asSequence()
-      .take(lastN)
-      .flatMap { message -> getFooterValues(footerType, message) }
-      .map(::CommitFooterValue)
-      .toList()
-
-    return if (isCoAuthoredBy) {
-      recentValues
-        .plus(defaultsService.getCoAuthors().take(3).map(::CommitFooterValue))
-        .distinctBy { it.text.toLowerCase() }
+  ): Collection<CommitFooterValue> =
+    if ("co-authored-by".equals(footerType, true)) {
+      defaultsService.getCoAuthors().take(3).map(::CommitFooterValue)
     } else {
-      recentValues.distinctBy { it.text.toLowerCase() }
+      emptyList()
     }
-  }
-
-  private fun getFooterValues(footerType: String, message: String): Sequence<String> =
-    message.replace(BEGIN_END_WS_REGEX, "")
-      .split(BLANK_LINES_REGEX)
-      .drop(1)
-      .asReversed()
-      .asSequence()
-      .map { it.replace(BEGIN_END_WS_REGEX, "") }
-      .filter(String::isNotBlank)
-      .map(CCParser::parseFooter)
-      .filter { footerType == (it.type as? ValidToken)?.value }
-      .map(FooterTokens::footer)
-      .filterIsInstance<ValidToken>()
-      .map(ValidToken::value)
-      .map(String::trim)
-      .filter(String::isNotEmpty)
 
   private fun notifyErrorToUser(e: Exception) {
     val message =
--- src/main/kotlin/com/github/lppedd/cc/api/DefaultVcsCommitSubjectProvider.kt
+++ src/main/kotlin/com/github/lppedd/cc/api/DefaultVcsCommitSubjectProvider.kt
@@ -3,16 +3,20 @@ package com.github.lppedd.cc.api
 import com.github.lppedd.cc.CCIcons
 import com.github.lppedd.cc.parser.CCParser
 import com.github.lppedd.cc.parser.CommitTokens
+import com.github.lppedd.cc.parser.FooterTokens
 import com.github.lppedd.cc.parser.ValidToken
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsConfiguration
 import org.jetbrains.annotations.ApiStatus.*
+import kotlin.text.RegexOption.MULTILINE
 
 /**
  * @author Edoardo Luppi
  */
 @Internal
-internal class DefaultVcsCommitSubjectProvider(private val project: Project) : CommitSubjectProvider {
+internal class DefaultVcsCommitSubjectProvider(private val project: Project)
+  : CommitSubjectProvider,
+    CommitFooterValueProvider {
   override fun getId(): String = ID
 
   override fun getPresentation(): ProviderPresentation =
@@ -34,7 +38,42 @@ internal class DefaultVcsCommitSubjectProvider(private val project: Project) : C
       .map(::CommitSubject)
       .toList()
 
+  override fun getCommitFooterValues(
+      footerType: String,
+      commitType: String?,
+      commitScope: String?,
+      commitSubject: String?,
+  ): Collection<CommitFooterValue> =
+    VcsConfiguration.getInstance(project)
+      .recentMessages
+      .asReversed()
+      .asSequence()
+      .take(if ("co-authored-by".equals(footerType, true)) 5 else 15)
+      .flatMap { message -> getFooterValues(footerType, message) }
+      .map(::CommitFooterValue)
+      .distinctBy { it.text.toLowerCase() }
+      .toList()
+
+  private fun getFooterValues(footerType: String, message: String): Sequence<String> =
+    message.replace(regexBeginEndWs, "")
+      .split(regexBlankLines)
+      .drop(1)
+      .asReversed()
+      .asSequence()
+      .map { it.replace(regexBeginEndWs, "") }
+      .filter(String::isNotBlank)
+      .map(CCParser::parseFooter)
+      .filter { footerType == (it.type as? ValidToken)?.value }
+      .map(FooterTokens::footer)
+      .filterIsInstance<ValidToken>()
+      .map(ValidToken::value)
+      .map(String::trim)
+      .filter(String::isNotEmpty)
+
   companion object {
     const val ID: String = "f3be5600-71b8-401c-bf50-e2465d8efca8"
+
+    private val regexBeginEndWs = Regex("""^\s+|\s+$""")
+    private val regexBlankLines = Regex("""^\s*$""", MULTILINE)
   }
 }
