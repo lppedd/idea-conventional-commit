e28085cfd043279516389424592dacfa82cc98eb
Edoardo Luppi <lp.edoardo@gmail.com>
2021-08-02 19:16:16 +0200

    refactor: replace in-code registry key registration with extension

--- src/main/kotlin/com/github/lppedd/cc/CCApplicationLoadListener.kt
+++ src/main/kotlin/com/github/lppedd/cc/CCApplicationLoadListener.kt
@@ -1,6 +1,5 @@
 package com.github.lppedd.cc
 
-import com.github.lppedd.cc.CCRegistry.RegistryKeyDescriptor
 import com.intellij.ide.ApplicationLoadListener
 import com.intellij.ide.plugins.PluginInstaller
 import com.intellij.openapi.application.Application
@@ -13,7 +12,6 @@ import com.intellij.openapi.project.ProjectManager
 private class CCApplicationLoadListener : ApplicationLoadListener {
   override fun beforeApplicationLoaded(application: Application, configPath: String) {
     subscribeToProjectOpened(application)
-    addRegistryKeys()
     PluginInstaller.addStateListener(CCPluginUninstallListener())
   }
 
@@ -22,18 +20,6 @@ private class CCApplicationLoadListener : ApplicationLoadListener {
     application.messageBus.connect(application).subscribe(
         ProjectManager.TOPIC,
         CCProjectManagerListener(),
-    )
-  }
-
-  private fun addRegistryKeys() {
-    CCRegistry.addKeys(
-        RegistryKeyDescriptor(
-            name = CC.Registry.VcsEnabled,
-            description = "Enable/disable the new VCS Provider",
-            defaultValue = "true",
-            restartRequired = true,
-            pluginId = CC.PluginId,
-        ),
     )
   }
 }
