214ae95aef7b3f4dc6ccf00ec35912879e2d2ee1
Edoardo Luppi <lp.edoardo@gmail.com>
2022-07-10 12:12:57 +0200

    build: update IntelliJ Gradle plugin to 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
   java
-  id("org.jetbrains.intellij") version "1.6.0"
+  id("org.jetbrains.intellij") version "1.7.0"
   id("org.jetbrains.grammarkit") version "2021.2.2"
   kotlin("jvm") version "1.7.10"
 }
