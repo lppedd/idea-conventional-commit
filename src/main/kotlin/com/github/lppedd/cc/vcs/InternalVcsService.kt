d51f2a20f08d8574dde76a318be3c14bc7f9ae1b
Brice Dutheil <brice.dutheil@gmail.com>
2023-03-21 12:27:45 +0100

    feature: Remove reflection and unused code
    Refs: #116

--- src/main/kotlin/com/github/lppedd/cc/vcs/InternalVcsService.kt
+++ src/main/kotlin/com/github/lppedd/cc/vcs/InternalVcsService.kt
@@ -11,7 +11,6 @@ import com.intellij.util.Consumer
 import com.intellij.util.EmptyConsumer
 import com.intellij.vcs.log.*
 import com.intellij.vcs.log.data.VcsLogMultiRepoJoiner
-import com.intellij.vcs.log.impl.VcsLogContentUtil
 import com.intellij.vcs.log.impl.VcsLogManager
 import com.intellij.vcs.log.impl.VcsProjectLog
 import com.intellij.vcs.log.visible.filters.VcsLogFilterObject
@@ -126,26 +125,9 @@ internal class InternalVcsService(private val project: Project) : VcsService {
       root: VirtualFile,
       commitsHashes: List<String>,
   ): List<VcsCommitMetadata> {
-    VcsLogProvider::class.java.methods.find { it.name == "readMetadata" }?.also {
-      it.isAccessible = true
-
-      when {
-        // 203.4203.26+
-        // (VirtualFile, List<String>, Consumer<? super VcsCommitMetadata>) -> void
-        it.returnType == Void.TYPE && it.parameterCount == 3 -> {
-          val commitsMetadata = ArrayList<VcsCommitMetadata>(100)
-          it.invoke(this, root, commitsHashes, Consumer(commitsMetadata::add))
-          return commitsMetadata
-        }
-        // (VirtualFile, List<String>) -> List<VcsCommitMetadata>
-        it.returnType == List::class.java && it.parameterCount == 2 -> {
-          @Suppress("unchecked_cast")
-          return it.invoke(this, root, commitsHashes) as List<VcsCommitMetadata>
-        }
-      }
+    return ArrayList<VcsCommitMetadata>(100).also {
+      readMetadata(root, commitsHashes, Consumer(it::add))
     }
-
-    return emptyList()
   }
 
   private fun getVcsLogProviders(): Map<VirtualFile, VcsLogProvider> {
@@ -179,24 +161,6 @@ internal class InternalVcsService(private val project: Project) : VcsService {
     }
 
     return commitsMetadata
-  }
-
-  @Suppress("unused")
-  @Compatibility(minVersion = "203.3645.34")
-  private fun ensureLogCreated(): Boolean {
-    val method =
-      VcsProjectLog::class.java.getDeclaredMethod("ensureLogCreated", Project::class.java)   // IDEA 203.3645.34+
-      ?: VcsProjectLog::class.java.getDeclaredMethod("getOrCreateLog", Project::class.java)  // IDEA 201.3803.32+
-      ?: VcsLogContentUtil::class.java.getDeclaredMethod("getOrCreateLog", Project::class.java)
-      ?: return false
-
-    return try {
-      method.isAccessible = true
-      method.invoke(null, project)
-      true
-    } catch (_: Exception) {
-      false
-    }
   }
 
   private inner class MyVcsLogRefresher : VcsLogRefresher {
