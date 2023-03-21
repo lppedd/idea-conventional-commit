4ec61e8349165ed0c3f6dfe94bc8e878eb03dbee
Brice Dutheil <brice.dutheil@gmail.com>
2023-03-21 11:27:08 +0100

    feature: Use notification group manager
    Fixes the warning:
    
    ```
    'constructor NotificationGroup(String, NotificationDisplayType, Boolean = ..., String? = ..., Icon? = ...)' is deprecated. Use com.intellij.notification.impl.NotificationGroupEP and com.intellij.notification.NotificationGroupManager
    ```
    
    Refs: #116

--- src/main/kotlin/com/github/lppedd/cc/CCNotificationService.kt
+++ src/main/kotlin/com/github/lppedd/cc/CCNotificationService.kt
@@ -4,21 +4,17 @@ import com.github.lppedd.cc.annotation.Compatibility
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 
 /**
  * @author Edoardo Luppi
  */
 internal object CCNotificationService {
-  @Compatibility(
-      minVersion = "203.3645.34",
-      replaceWith = "NotificationGroupEP/NotificationGroupManager"
-  )
-  private val notificationGroup = NotificationGroup(
-      "com.github.lppedd.cc.notifications",
-      NotificationDisplayType.STICKY_BALLOON,
-      true
-  )
+  private const val CC_GROUP = "com.github.lppedd.cc.notifications"
+
+  private val notificationGroup = NotificationGroupManager.getInstance()
+    .getNotificationGroup(CC_GROUP)
 
   fun createErrorNotification(message: String): Notification =
     notificationGroup.createNotification("Conventional Commit", message, NotificationType.ERROR)
