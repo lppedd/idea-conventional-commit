9adaf4e92fa57129b972669be8227c50c9c19e01
Brice Dutheil <brice.dutheil@gmail.com>
2023-03-21 11:39:54 +0100

    feature: Replace deprecated ActionLink
    Fixes the warning:
    
    ```
    'ActionLink' is deprecated. Deprecated in Java
    ```
    
    Refs: #116

--- src/main/kotlin/com/github/lppedd/cc/whatsnew/WhatsNewEmptyPanel.kt
+++ src/main/kotlin/com/github/lppedd/cc/whatsnew/WhatsNewEmptyPanel.kt
@@ -1,14 +1,13 @@
 package com.github.lppedd.cc.whatsnew
 
 import com.github.lppedd.cc.CCBundle
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.ide.BrowserUtil
 import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.labels.ActionLink
+import com.intellij.ui.components.labels.LinkLabel
+import com.intellij.ui.components.labels.LinkListener
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import java.awt.Component
-import java.awt.Desktop
 import java.net.URI
 import javax.swing.Box
 import javax.swing.BoxLayout
@@ -24,8 +23,8 @@ internal class WhatsNewEmptyPanel : JPanel() {
       it.foreground = UIUtil.getInactiveTextColor()
     }
 
-    val link = ActionLink(CCBundle["cc.whatsnew.dialog.visitPage"], VisitPageAction()).also {
-      it.alignmentX = Component.CENTER_ALIGNMENT
+    val link = LinkLabel(CCBundle["cc.whatsnew.dialog.visitPage"], null, VisitPageAction()).also {
+      it.alignmentX = CENTER_ALIGNMENT
       it.setPaintUnderline(false)
     }
 
@@ -37,13 +36,9 @@ internal class WhatsNewEmptyPanel : JPanel() {
     border = JBUI.Borders.empty(40, 30)
   }
 
-  private class VisitPageAction : AnAction() {
-    override fun actionPerformed(event: AnActionEvent) {
-      try {
-        Desktop.getDesktop().browse(URI(CCBundle["cc.plugin.repository"]))
-      } catch (_: Exception) {
-        //
-      }
+  private class VisitPageAction : LinkListener<Any> {
+    override fun linkSelected(aSource: LinkLabel<Any>?, aLinkData: Any?) {
+      BrowserUtil.browse(URI(CCBundle["cc.plugin.repository"]))
     }
   }
 }
