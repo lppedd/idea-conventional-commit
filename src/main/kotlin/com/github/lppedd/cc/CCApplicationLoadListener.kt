76c0b7a2a8dded61c8abce0669b1e6a906c2de77
Edoardo Luppi <lp.edoardo@gmail.com>
2024-01-21 17:26:42 +0100

    refactor: make CCApplicationLoadListener compatible with 2024.1

--- src/main/kotlin/com/github/lppedd/cc/CCApplicationLoadListener.kt
+++ src/main/kotlin/com/github/lppedd/cc/CCApplicationLoadListener.kt
@@ -1,5 +1,6 @@
 package com.github.lppedd.cc
 
+import com.github.lppedd.cc.annotation.Compatibility
 import com.intellij.ide.ApplicationLoadListener
 import com.intellij.ide.plugins.PluginInstaller
 import com.intellij.openapi.application.Application
@@ -10,7 +11,14 @@ import java.nio.file.Path
  */
 @Suppress("UnstableApiUsage")
 internal class CCApplicationLoadListener : ApplicationLoadListener {
+  @Compatibility(minVersion = "241.8102.112")
   override fun beforeApplicationLoaded(application: Application, configPath: Path) {
+    PluginInstaller.addStateListener(CCPluginStateListener())
+  }
+
+  @JvmName("beforeApplicationLoaded")
+  @Suppress("unused", "unused_parameter", "RedundantSuspendModifier")
+  suspend fun beforeApplicationLoadedCompat(application: Application, configPath: Path) {
     PluginInstaller.addStateListener(CCPluginStateListener())
   }
 }
