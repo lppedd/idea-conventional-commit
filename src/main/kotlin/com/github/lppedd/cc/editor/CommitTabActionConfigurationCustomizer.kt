b88c0e7ad47b3ee9e95223c09c84e00fdb3bcb32
Edoardo Luppi <lp.edoardo@gmail.com>
2026-01-07 17:45:43 +0100

    refactor: replace deprecated ActionConfigurationCustomizer.customize with CustomizeStrategy

--- src/main/kotlin/com/github/lppedd/cc/editor/CommitTabActionConfigurationCustomizer.kt
+++ src/main/kotlin/com/github/lppedd/cc/editor/CommitTabActionConfigurationCustomizer.kt
@@ -1,38 +1,38 @@
 package com.github.lppedd.cc.editor
 
 import com.intellij.ide.plugins.PluginManager
-import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.Constraints
 import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.ex.ActionRuntimeRegistrar
 import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer
+import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer.LightCustomizeStrategy
 import com.intellij.openapi.extensions.PluginId
 
 /**
  * @author Edoardo Luppi
  */
 @Suppress("UnstableApiUsage")
-internal class CommitTabActionConfigurationCustomizer : ActionConfigurationCustomizer {
+internal class CommitTabActionConfigurationCustomizer : ActionConfigurationCustomizer, LightCustomizeStrategy {
   private val actionId = "EditorTab"
   private val groupId = "EditorActions"
 
-  @Deprecated("Implement one of [CustomizeStrategy]")
-  override fun customize(actionManager: ActionManager) {
+  override suspend fun customize(actionRegistrar: ActionRuntimeRegistrar) {
     // Rider register its own EditorTab action, see com.jetbrains.rider.editorActions.FrontendTabAction.
     // Unfortunately, that action is coded in Kotlin and marked as final,
     // so the only way to avoid breaking Rider is simply to not offer
     // enhanced tabbing in the commit dialog
-    if (!PluginManager.isPluginInstalled(PluginId.getId("com.intellij.modules.rider"))) {
-      val oldAction = actionManager.getActionOrStub(actionId)
+    if (PluginManager.isPluginInstalled(PluginId.getId("com.intellij.modules.rider"))) {
+      return
+    }
 
-      if (oldAction != null) {
-        val actionGroup = actionManager.getAction(groupId) as DefaultActionGroup
-        val newAction = CommitTabAction()
-        actionManager.unregisterAction(actionId)
-        actionManager.registerAction(actionId, newAction)
+    val oldAction = actionRegistrar.getActionOrStub(actionId)
 
-        // The input ActionManager must be passed in to avoid infinite recursion
-        actionGroup.addAction(newAction, Constraints.LAST, actionManager)
-      }
+    if (oldAction != null) {
+      val actionGroup = actionRegistrar.getActionOrStub(groupId) as DefaultActionGroup
+      val newAction = CommitTabAction()
+      actionRegistrar.unregisterAction(actionId)
+      actionRegistrar.registerAction(actionId, newAction)
+      actionRegistrar.addToGroup(actionGroup, newAction, Constraints.LAST)
     }
   }
 }
